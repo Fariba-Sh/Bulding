0d02412042f62f598290a2bf3ab3b1b6c4793f6b
Fariba <faribashakeri81@gmail.com>
2025-09-03 20:20:34 +0330

    templates added and user login is made

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,5 +1,49 @@
-from flask import Blueprint , render_template
-
-
+from flask import Blueprint , render_template , request ,redirect,url_for , flash
+from flask_login import login_user,login_required,current_user, logout_user
+from models.user import *
+from extentions import db
+from passlib.hash import sha256_crypt
 
 app = Blueprint("user" , __name__)
+
+@app.route("/user/login" ,methods = ['GET','POST'])
+def user_login():
+    if request.method == 'GET':
+        if current_user.is_authenticated:
+            return redirect(url_for('user.dashboard')) 
+        return render_template("user/login.html")
+    else:
+        register = request.form.get('register', None)
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        phone = request.form.get('phone', None)
+        address = request.form.get('address', None)
+
+        if register !=None:
+            user = User.query.filter(User.username == username).first()
+            if user != None:
+                flash('نام کاربری دیگری انتخاب کنید')
+                return redirect(url_for('user.login'))
+            
+
+
+            user = User(username= username , password = sha256_crypt.encrypt(password), phone = phone, address=address )
+            db.session.add (user)
+            db.session.commit()
+            login_user(user)
+
+            return redirect(url_for('user.dashboard'))
+        
+        else:
+            user = User.query.filter(User.username == username).first()
+            if user == None:
+                flash('نام کاربری یا رمز اشتباه است')
+                return redirect(url_for('user.login'))
+            
+            if sha256_crypt.verify(password , user.password):
+                login_user(user)
+                return redirect(url_for('user.dashboard'))
+            else:
+                flash('نام کاربری یا رمز اشتباه است')
+                return redirect(url_for('user.login'))
+        
---
+++ models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy import *
+from extentions import db
+from flask_login import UserMixin
+
+
+class User (db.Model ,UserMixin):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(200), nullable=False , unique=True, index=True)
+    password = Column(String, nullable=False , index=True)
+    phone = Column(String(11) ,nullable=False ,index=True)

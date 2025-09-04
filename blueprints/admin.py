df82344460944d5bb8cae1e284eca43370429f04
Fariba <faribashakeri81@gmail.com>
2025-09-04 10:45:01 +0330

    admin login has been made

--- blueprints/admin.py
+++ blueprints/admin.py
@@ -1,5 +1,36 @@
-from flask import Blueprint , render_template
-
+from flask import Blueprint ,redirect,url_for, render_template,session,abort,request
+import config
 
 
 app = Blueprint("admin" , __name__)
+
+
+@app.before_request
+def before_request():
+       if session.get('admin_login' , None) == None and request.endpoint != "admin.admin_login" :
+        abort(403)
+        # abort 403 :forbiden
+
+
+
+@app.route('/admin/login' , methods = ["POST" , "GET"])
+def admin_login():
+    if request.method == "POST":
+        username = request.form.get("username", None)
+        password = request.form.get("password" , None)
+    
+        if username == config.ADMIN_USERNAME and password == config.ADMIN_PASSWORD :
+            session['admin_login'] = username
+            return redirect(url_for("admin.admin_dashboard"))
+        else:
+
+            return redirect(url_for("admin.admin_login"))
+
+    
+    else:
+        return render_template("admin/login.html")
+
+
+@app.route('/admin/dashboard' , methods = ["GET"])
+def admin_dashboard():
+    return render_template("admin/dashboard.html")
--- config.py
+++ config.py
@@ -2,3 +2,8 @@ SECRET_KEY = "DLOHNCFDSHCHSDGBHSVHJubguishhcsbvgfyusggT!@@!@@55565656r56454r"
 
 
 SQLALCHEMY_DATABASE_URI = "sqlite:///Building1.db"
+
+
+ADMIN_USERNAME = 'fari_sh_aqw'
+
+ADMIN_PASSWORD = 'hdejkfnfeujrfeufcbcb67644'
